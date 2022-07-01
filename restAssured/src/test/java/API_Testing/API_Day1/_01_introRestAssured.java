9d5c8d9cbaeeb78f4366eff90ee77a069f33cb98
kalyaratR <kitty25242009@hotmail.com>
2022-06-30 23:20:21 -0400

    newupdate

--- restAssured/src/test/java/API_Testing/API_Day1/_01_introRestAssured.java
+++ restAssured/src/test/java/API_Testing/API_Day1/_01_introRestAssured.java
@@ -5,26 +5,27 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 public class _01_introRestAssured {
+
     // Rest Assured is a java library that is used to perform API tests
-// It uses BDD Style such as given, when, then, and, etc.
-// Rest Assured has methods to fetch data from the response body. When making a request.
-// Rest Assured works with HTTP requests: CRUD (Create, Read, Update, Delete) (POST, GET, PUT, DELETE)
-// As testers we mostly use 2 requests GET, POST among DELETE and PUT.
+    // It uses BDD Style such as given, when, then, and, etc.
+    // Rest Assured has methods to fetch data from the response body. When making a request.
+    // Rest Assured works with HTTP requests: CRUD (Create, Read, Update, Delete) (POST, GET, PUT, DELETE)
+    // As testers we mostly use 2 requests GET, POST among DELETE and PUT.
 
-// NOTE: As testers we mostly work with JSON Payloads (BODY), we also experience XML files, text, pdf etc.
+    // NOTE: As testers we mostly work with JSON Payloads (BODY), we also experience XML files, text, pdf etc.
 
-// Some of the most used methods in RestAssured are:
-//  When making a request:
-//      given() ---> used to prepare the request
-//      when() ---> used to send the request
-//      then() ---> used to verify the request
-//  When we verify the response:
-//      prettyPeek() ---> used to print the response headers, urls etc. in pretty format in the Console
-//      prettyPrint() ---> used to print the response in pretty format
-//      log() ---> logs(prints) all the response
-//      asString() ---> allows us to print in String format
-//      contentType() ---> used to verify the content type from the response body. When use POST request
-//      accept() ---> used to verify body response from the header when making a GET request
+    // Some of the most used methods in RestAssured are:
+    //  When making a request:
+    //      given() ---> used to prepare the request
+    //      when() ---> used to send the request
+    //      then() ---> used to verify the request
+    //  When we verify the response:
+    //      prettyPeek() ---> used to print the response headers, urls etc. in pretty format in the Console
+    //      prettyPrint() ---> used to print the response in pretty format
+    //      log() ---> logs(prints) all the response
+    //      asString() ---> allows us to print in String format
+    //      contentType() ---> used to verify the content type from the response body. When use POST request
+    //      accept() ---> used to verify body response from the header when making a GET request
 
     //      baseURI ---> used to save the base URL for all the resources
     public static String baseURI = "https://api.octoperf.com";
@@ -32,70 +33,73 @@ public class _01_introRestAssured {
     //  When we make a request we only provide the path(endpoint) to a specific baseURI
     private String path = "public/users/login";
 
-// FULL URL = https://api.octoperf.com/public/users/login
+    // FULL URL = https://api.octoperf.com/public/users/login
 
-// What is an endpoint?
-// An endpoint is a unique URL that represents and object or collections of Objects
+    // What is an endpoint?
+    // An endpoint is a unique URL that represents and object or collections of Objects
 
-// Example: http://www.google.com/search?source=JavaBook
-//                  baseURI       Endpoint  ? parameter
+    // Example: http://www.google.com/search?source=JavaBook
+    //                  baseURI       Endpoint  ? parameter
 
-// Base url = https://api.octoperf.com
-// Full url = https://api.octoperf.com/public/users/login
-// Full url with Query params =
-//          https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com
+    // Base url = https://api.octoperf.com
+    // Full url = https://api.octoperf.com/public/users/login
+    // Full url with Query params =
+    //          https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com
 
 
-// Task: Make a HTTP: POST request with given
-// full url with query params and print the response
+    // Task: Make a HTTP: POST request with given
+    // full url with query params and print the response
 
-@Test
+    @Test
     public void printResponse(){
-    RestAssured.given()
-            .when()
-            .post("https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com")
-            .prettyPeek(); // Prints the whole response body and headers
-}
-@Test
-    public void printResponsePrettyPrint(){
-    RestAssured.given()
-            .when()
-            .post("https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com")
-            .prettyPrint(); // Prints the whole response body and headers
-}
-/*
-When we verify the status codes we must pay attention to the following errors from the response body
+        RestAssured.given()
+                .when()
+                .post("https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com")
+                .prettyPeek(); // Prints the whole response body and headers
+    }
 
-1xx --> Information
-2xx --> success
- */
+    @Test
+    public void printResponsePrettyPrint(){
+        RestAssured.given()
+                .when()
+                .post("https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com")
+                .prettyPrint(); // Prints the whole response body and headers
+    }
+
+    /*
+    When we verify the status codes we must pay attention to the following errors from the response body
+
+    1xx --> Information
+    2xx --> success ( 200 -> OK, 201 -> Created, 202 -> Accepted, 204 -> No content)
+    3xx --> Redirection
+    4xx --> Client error (400 -> Bad Request, 401 -> Unauthorized, 403 -> Forbidden, 404 -> Not Found)
+    5xx --> Server error (500 -> Internal Server Error)
+     */
+
     // Perform a Post request to log in API endpoint
     // Verify Status code is 200 OK
 
-@Test
-public void verifyStatusCode(){
-    RestAssured.given()
-            .when()
-            .post("https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com")
-            .then()
-            .assertThat()
-            .statusCode(200);
-    System.out.println("Test verified, status code is 200 ok");// Optional
-}
-    // Perform a Post request to log in API endpoint
-    // Verify Content Type = JSON
-
     @Test
-    public void verifyStatusType(){
+    public void verifyStatusCode(){
         RestAssured.given()
                 .when()
                 .post("https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com")
                 .then()
                 .assertThat()
-                .contentType(ContentType.JSON);
-        System.out.println("Test verify that content type = JSON");
+                .statusCode(200);
+        System.out.println("Test verified, status code is 200 ok"); // Optional
+    }
+
+    // Perform a Post request to log in API endpoint
+    // Verify Content Type = JSON
+    @Test
+    public void verifyContentType(){
+        RestAssured.given()
+                .when()
+                .post("https://api.octoperf.com/public/users/login?password=test12&username=tla.jiraone@gmail.com")
+                .then()
+                .assertThat()
+                .contentType(ContentType.XML);
+        System.out.println("Test verified that content type = JSON");
     }
 }
-
-
-
---
+++ restAssured/src/test/java/API_Testing/StudentPractice/api_Activities.java
@@ -0,0 +1,23 @@
+package API_Testing.StudentPractice;
+
+import org.testng.annotations.Test;
+
+public class api_Activities {
+
+    // Testing URI = https://fakerestapi.azurewebsites.net/api/v1/
+
+    // Task 1: Using Rest Assured validate the status code for endpoint /Activities
+    @Test
+    public void verifyStatusCode(){
+
+    }
+
+    // Task 2: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
+    // for endpoint /Activities
+
+
+    // Task 1: Using Rest Assured validate the status code for endpoint /Activities/12
+
+    // Task 2: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
+    // for endpoint /Activities/12
+}
