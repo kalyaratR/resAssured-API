0a11a02c4c6558f3d1880157c631e023a397e0f3
kalyaratR <kitty25242009@hotmail.com>
2022-07-01 18:01:01 -0400

    newadd

--- restAssured/src/test/java/API_Testing/StudentPractice/api_Activities.java
+++ restAssured/src/test/java/API_Testing/StudentPractice/api_Activities.java
@@ -1,23 +1,49 @@
 package API_Testing.StudentPractice;
 
+import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 public class api_Activities {
 
     // Testing URI = https://fakerestapi.azurewebsites.net/api/v1/
 
-    // Task 1: Using Rest Assured validate the status code for endpoint /Activities
+    // Task 1: Using Rest Assured validate the status code for endpoint /Activities GET
     @Test
-    public void verifyStatusCode(){
-
+    public void verifyStatusCodeActivities(){
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
+                .then()
+                .statusCode(200);
     }
-
     // Task 2: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
     // for endpoint /Activities
-
+    @Test
+    public void verifyActivitiesContentTypeOption2(){
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
+                .then()
+                .assertThat().header("Content-Type", "application/json; charset=utf-8; v=1.0");
+    }
 
     // Task 1: Using Rest Assured validate the status code for endpoint /Activities/12
-
-    // Task 2: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
-    // for endpoint /Activities/12
+    @Test
+    public void verifyStatusCodeAuthors(){
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Authors")
+                .then()
+                .statusCode(200);
+    }
+    // Task 2: Using Rest Assured Content-Type  is application/json; charset=utf-8; v=1.0
+    // for endpoint /Activities/10
+    @Test
+    public void verifyContentTypeOption2Authors(){
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Authors/authors/books/10")
+                .then()
+                .statusCode(200);
+    }
 }
